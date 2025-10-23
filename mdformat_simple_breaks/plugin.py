43cb90a0481e04a5f22fd9197a678d0a2124d691
Kevin Deldycke <kevin@deldycke.com>
2025-10-23 17:44:12 +0400

    Defer typing imports

--- mdformat_simple_breaks/plugin.py
+++ mdformat_simple_breaks/plugin.py
@@ -1,10 +1,14 @@
 """Main plugin module."""
 
-from typing import Mapping
+from __future__ import annotations
 
-from markdown_it import MarkdownIt
-from mdformat.renderer import RenderContext, RenderTreeNode
-from mdformat.renderer.typing import Render
+TYPE_CHECKING = False
+if TYPE_CHECKING:
+    from collections.abc import Mapping
+
+    from markdown_it import MarkdownIt
+    from mdformat.renderer import RenderContext, RenderTreeNode
+    from mdformat.renderer.typing import Render
 
 
 def update_mdit(mdit: MarkdownIt) -> None:
